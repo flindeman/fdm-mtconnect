16c03890908c312dc9f8637312203572b571e50e
Frank Lindeman <frank@grabcad.com>
2018-11-02 12:10:09 +0000

    Give the various controls more space so it's easier to read.

--- Samples/SampleClient/SampleClient/MainForm.Designer.cs
+++ Samples/SampleClient/SampleClient/MainForm.Designer.cs
@@ -35,18 +35,18 @@
             this.agentAddress = new System.Windows.Forms.TextBox();
             this.connect = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.propertyList = new System.Windows.Forms.ListView();
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dataPlot = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.recordData = new System.Windows.Forms.CheckBox();
             this.dataList = new System.Windows.Forms.ListView();
             this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.propertyList = new System.Windows.Forms.ListView();
-            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader7 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.recordData = new System.Windows.Forms.CheckBox();
+            this.dataPlot = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataPlot)).BeginInit();
@@ -54,13 +54,13 @@
             // 
             // agentTree
             // 
-            this.agentTree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.agentTree.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.agentTree.HideSelection = false;
             this.agentTree.Location = new System.Drawing.Point(6, 19);
             this.agentTree.Name = "agentTree";
-            this.agentTree.Size = new System.Drawing.Size(242, 273);
+            this.agentTree.Size = new System.Drawing.Size(445, 263);
             this.agentTree.TabIndex = 0;
             // 
             // label1
@@ -100,35 +100,46 @@
             this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.Location = new System.Drawing.Point(12, 32);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(254, 445);
+            this.groupBox1.Size = new System.Drawing.Size(457, 573);
             this.groupBox1.TabIndex = 4;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Agent Structure";
+            // 
+            // propertyList
+            // 
+            this.propertyList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader4,
+            this.columnHeader5});
+            this.propertyList.GridLines = true;
+            this.propertyList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.propertyList.Location = new System.Drawing.Point(6, 288);
+            this.propertyList.Name = "propertyList";
+            this.propertyList.Size = new System.Drawing.Size(445, 268);
+            this.propertyList.TabIndex = 1;
+            this.propertyList.UseCompatibleStateImageBehavior = false;
+            this.propertyList.View = System.Windows.Forms.View.Details;
             // 
             // groupBox2
             // 
             this.groupBox2.Controls.Add(this.recordData);
             this.groupBox2.Controls.Add(this.dataList);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.groupBox2.Location = new System.Drawing.Point(272, 221);
+            this.groupBox2.Location = new System.Drawing.Point(490, 323);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(500, 256);
+            this.groupBox2.Size = new System.Drawing.Size(629, 282);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Data";
             // 
-            // dataPlot
+            // recordData
             // 
-            this.dataPlot.AllowDrop = true;
-            chartArea1.Name = "ChartArea1";
-            this.dataPlot.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.dataPlot.Legends.Add(legend1);
-            this.dataPlot.Location = new System.Drawing.Point(272, 51);
-            this.dataPlot.Name = "dataPlot";
-            this.dataPlot.Size = new System.Drawing.Size(500, 164);
-            this.dataPlot.TabIndex = 6;
-            this.dataPlot.Text = "chart1";
+            this.recordData.AutoSize = true;
+            this.recordData.Location = new System.Drawing.Point(7, 23);
+            this.recordData.Name = "recordData";
+            this.recordData.Size = new System.Drawing.Size(263, 21);
+            this.recordData.TabIndex = 1;
+            this.recordData.Text = "Record data to \'C:\\mtc_data.csv\'";
+            this.recordData.UseVisualStyleBackColor = true;
             // 
             // dataList
             // 
@@ -140,9 +151,9 @@
             this.columnHeader6,
             this.columnHeader7});
             this.dataList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
-            this.dataList.Location = new System.Drawing.Point(7, 53);
+            this.dataList.Location = new System.Drawing.Point(5, 53);
             this.dataList.Name = "dataList";
-            this.dataList.Size = new System.Drawing.Size(487, 197);
+            this.dataList.Size = new System.Drawing.Size(594, 212);
             this.dataList.TabIndex = 0;
             this.dataList.UseCompatibleStateImageBehavior = false;
             this.dataList.View = System.Windows.Forms.View.Details;
@@ -150,53 +161,45 @@
             // columnHeader1
             // 
             this.columnHeader1.Text = "category";
+            this.columnHeader1.Width = 106;
             // 
             // columnHeader2
             // 
             this.columnHeader2.Text = "id";
+            this.columnHeader2.Width = 88;
             // 
             // columnHeader3
             // 
             this.columnHeader3.Text = "name";
-            // 
-            // propertyList
-            // 
-            this.propertyList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeader4,
-            this.columnHeader5});
-            this.propertyList.GridLines = true;
-            this.propertyList.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
-            this.propertyList.Location = new System.Drawing.Point(6, 298);
-            this.propertyList.Name = "propertyList";
-            this.propertyList.Size = new System.Drawing.Size(242, 141);
-            this.propertyList.TabIndex = 1;
-            this.propertyList.UseCompatibleStateImageBehavior = false;
-            this.propertyList.View = System.Windows.Forms.View.Details;
+            this.columnHeader3.Width = 84;
             // 
             // columnHeader6
             // 
             this.columnHeader6.Text = "type";
+            this.columnHeader6.Width = 82;
             // 
             // columnHeader7
             // 
             this.columnHeader7.Text = "value";
             // 
-            // recordData
+            // dataPlot
             // 
-            this.recordData.AutoSize = true;
-            this.recordData.Location = new System.Drawing.Point(7, 23);
-            this.recordData.Name = "recordData";
-            this.recordData.Size = new System.Drawing.Size(263, 21);
-            this.recordData.TabIndex = 1;
-            // [TODO] remove or use actual value.
-            this.recordData.Text = "Record data to \'C:\\mtc_data.csv\'";
-            this.recordData.UseVisualStyleBackColor = true;
+            this.dataPlot.AllowDrop = true;
+            chartArea1.Name = "ChartArea1";
+            this.dataPlot.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.dataPlot.Legends.Add(legend1);
+            this.dataPlot.Location = new System.Drawing.Point(490, 51);
+            this.dataPlot.Name = "dataPlot";
+            this.dataPlot.Size = new System.Drawing.Size(629, 263);
+            this.dataPlot.TabIndex = 6;
+            this.dataPlot.Text = "chart1";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(778, 482);
+            this.ClientSize = new System.Drawing.Size(1145, 617);
             this.Controls.Add(this.dataPlot);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
